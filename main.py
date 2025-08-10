77daf1dd6947f74a698afc36b9a1b12e352b83a7
Raphael Fernandes Franca <116182607+RaphaelFF@users.noreply.github.com>
2025-08-10 20:14:01 -0300

    feat: adicionando botão para acessar painel ADM

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# main.py
 import streamlit as st
 from paginas.adm import interface_adm
 from paginas.usuario import interface_usuario_comum
@@ -6,23 +7,32 @@ from paginas.usuario import interface_usuario_comum
 SENHA_ADM = "admin123"
 
 def main():
- 
-    # Usando st.session_state para manter o estado de acesso do ADM
+    st.sidebar.title("Navegação")
+
     if 'acesso_adm' not in st.session_state:
         st.session_state.acesso_adm = False
-
-    st.sidebar.title("Navegação")
+    
+    if 'mostrar_login_adm' not in st.session_state:
+        st.session_state.mostrar_login_adm = False
 
     if st.session_state.acesso_adm:
         if st.sidebar.button("Voltar para Usuário Comum"):
             st.session_state.acesso_adm = False
+            st.session_state.mostrar_login_adm = False
         interface_adm()
     else:
-        senha = st.sidebar.text_input("Digite a senha ADM", type="password")
-        if senha == SENHA_ADM:
-            st.session_state.acesso_adm = True
-        elif senha:
-            st.sidebar.error("Senha incorreta.")
+        if st.sidebar.button("Acesso ADM"):
+            st.session_state.mostrar_login_adm = True
+
+        if st.session_state.mostrar_login_adm:
+            senha = st.sidebar.text_input("Digite a senha ADM", type="password")
+            if senha == SENHA_ADM:
+                st.session_state.acesso_adm = True
+                st.session_state.mostrar_login_adm = False 
+                st.experimental_rerun() 
+            elif senha:
+                st.sidebar.error("Senha incorreta.")
+
         interface_usuario_comum()
 
 if __name__ == "__main__":
